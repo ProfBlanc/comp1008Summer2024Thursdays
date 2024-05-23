40e2cb4b5301b408132522e9aec1ed7e0a7016b2
Ben <ben.blanc@georgebrown.ca>
2024-05-23 12:02:15 -0400

    Class Code May 23, 2024 part 1

---
+++ src/intro/Week3.java
@@ -0,0 +1,25 @@
+package intro;
+
+public class Week3 {
+
+    static void workingWithArrays(){
+
+        int[] nums1 = {11,22,33,44,55};
+        int[] nums2 = new int[5]; // all indexes have value 0
+
+        //output:
+        System.out.println(nums1[2]);
+        /*
+indexes            0       1       2       3       4
+
+values              11      22      33      44      55
+         */
+        String[] texts = new String[5];//all indexes null
+
+    int val = "hello".length(); // gets length of string
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
